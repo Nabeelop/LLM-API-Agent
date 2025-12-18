401b61514e8f54552641c4ef0e44772a0fa4da5e
Nabeel <smnrizvi2306@gmail.com>
2025-12-18 18:20:30 +0530

    added custom API splitter

---
+++ rag/splitter.py
@@ -0,0 +1,30 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+#Splitting the document into chunks for better performance
+def split_docs(docs):
+    api_separators = [
+    "\n## ",
+    "\n### ",
+    "\n#### ",
+    "\nEndpoint:",
+    "\nMethod:",
+    "\nRequest",
+    "\nResponse",
+    "\nParameters",
+    "\nHeaders",
+    "\nBody",
+    "\n```",
+    "\n\n",
+    "\n",
+    " ",
+    ""
+    ]
+
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100,
+    separators=api_separators
+    )
+    
+    chunks=text_splitter(docs)
+
+    return chunks
